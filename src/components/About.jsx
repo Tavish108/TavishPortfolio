7df294742f6c7ba6bf97f210cfbaeb91ee4e4ea1
Tavish108 <sharmatavish318@gmail.com>
2024-12-27 19:37:19 +0530

    portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Tilt from "react-parallax-tilt"; // Updated import
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt
+    className='xs:w-[250px] w-full'
+    tiltMaxAngleX={45}
+    tiltMaxAngleY={45}
+    scale={1}
+    transitionSpeed={450}
+  >
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+       
+        <h2 className={styles.sectionHeadText}>About</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+       I am Tavish Sharma, a passionate tech enthusiast and a 3rd-year B.Tech student specializing in computer science at KIET. With a strong foundation in programming, web development, and AI, I bring innovative ideas to life through impactful projects. As the AR/VR Lead at Innogenks, my technical club, I mentor over 50 students, drive domain-specific training, and oversee cutting-edge initiatives.
+       I have a proven track record in hackathons, such as Hack the Mountain 5.0, where my team ranked among the top 40. My projects span from advanced web platforms to AI-based applications, demonstrating versatility and creativity. Additionally, I’ve contributed as an inventory manager, a rocket design intern, and a leader of pan-India events like Innohacks 3.0.
+
+I thrive on tackling challenges, sharing knowledge, and creating meaningful solutions that resonate across domains.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
