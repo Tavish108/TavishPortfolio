0fb1e42571176449ea9e8112cfd59e1867b21c0d
Tavish108 <123955740+Tavish108@users.noreply.github.com>
2024-12-27 20:03:31 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Tavish Sharma</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am Tech enthusiast, Web Developer and  problem-solver <br className='sm:block hidden' />
-            and a 
+            I am Tech enthusiast, Web Developer and problem-solver! <br className='sm:block hidden' />
+    
           </p>
         </div>
       </div>
